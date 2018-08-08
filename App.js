24501b0bcc57d3472cbc8b813b668f6e0d1e53db
FRAMGIA\nguyen.manh.duc <nguyen.manh.duc@framgia.com>
2018-08-08 17:52:57 +0700

    init

---
+++ App.js
@@ -0,0 +1,73 @@
+import React from 'react';
+import { StyleSheet, 
+  View, 
+  Animated, 
+  Dimensions,
+  TouchableWithoutFeedback,
+  Easing
+} from 'react-native';
+const { width, height } = Dimensions.get('window');
+const size = Math.min(width, height) - 1;
+export default class App extends React.Component { 
+  constructor(props) {
+    super(props);
+    this.state = {
+      scale: new Animated.Value(0)
+    };
+  }
+  
+  onPress() {
+    Animated.timing(this.state.scale, {
+      toValue: 4,
+      duration: 1000,
+      easing: Easing.linear
+    }).start(() => {
+      this.setState({
+        scale: new Animated.Value(0)
+      });
+    });
+  }
+
+  getLeftPosition () {
+    const halfSize = size / 2;
+    const halfWidth = width / 2;
+    let marginHorizontalTopLeft = -halfSize;
+  return marginHorizontalTopLeft + halfWidth;
+  }
+  getTopPosition () {
+    const halfSize = size / 2;
+    let marginVerticalTopLeft = -halfSize;
+  return marginVerticalTopLeft + height;
+  }
+
+  render() {
+    let topPosition = this.getTopPosition();
+    let leftPosition = this.getLeftPosition();
+    return (
+      <TouchableWithoutFeedback onPress={this.onPress.bind(this)}>
+      <View style={styles.container}>
+        <Animated.View style={{
+          position: 'absolute',
+          backgroundColor: '#ccc',
+          width: size,
+          height: size,
+          borderRadius: size / 2,
+          transform: [{
+            scale: this.state.scale
+          }],
+          left: leftPosition,
+          top: topPosition
+        }} />
+      </View>
+      </TouchableWithoutFeedback>
+    );
+  }
+}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#fff'
+  },
+});
