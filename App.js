3e3e8fab3a1f323f1c5f7b73c2168bc41321d0a5
FRAMGIA\nguyen.manh.duc <nguyen.manh.duc@framgia.com>
2018-08-08 18:19:59 +0700

    finally

--- App.js
+++ App.js
@@ -1,65 +1,94 @@
 import React from 'react';
-import { StyleSheet, 
-  View, 
-  Animated, 
+import {
+  StyleSheet,
+  View,
+  Animated,
   Dimensions,
   TouchableWithoutFeedback,
   Easing
 } from 'react-native';
+
+import GestureRecognizer, { swipeDirections } from 'react-native-swipe-gestures';
+import CircleTransition from './src/components/CircleTransition.js';
+
 const { width, height } = Dimensions.get('window');
-const size = Math.min(width, height) - 1;
-export default class App extends React.Component { 
+const screens = [{
+  id: 0,
+  bgcolor: '#698FB2'
+}, {
+  id: 1,
+  bgcolor: '#68B0B3'
+}, {
+  id: 2,
+  bgcolor: '#9B91BA'
+}];
+
+export default class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      scale: new Animated.Value(0)
+      _counter: 0,
+      currentbg: screens[0].bgcolor
     };
   }
-  
-  onPress() {
-    Animated.timing(this.state.scale, {
-      toValue: 4,
-      duration: 1000,
-      easing: Easing.linear
-    }).start(() => {
-      this.setState({
-        scale: new Animated.Value(0)
-      });
+
+  changeColor(newColor) {
+    this.setState({
+      currentbg: newColor
     });
   }
 
-  getLeftPosition () {
-    const halfSize = size / 2;
-    const halfWidth = width / 2;
-    let marginHorizontalTopLeft = -halfSize;
-  return marginHorizontalTopLeft + halfWidth;
+  onSwipeLeft() {
+    console.log("left");
+    const { _counter } = this.state;
+    let newCounter = _counter < screens.length - 1 ? _counter + 1 : 0;
+    this.swipeTo(newCounter);
   }
-  getTopPosition () {
-    const halfSize = size / 2;
-    let marginVerticalTopLeft = -halfSize;
-  return marginVerticalTopLeft + height;
+  onSwipeRight() {
+    console.log("right");
+    const { _counter } = this.state;
+    let newCounter = _counter === 0 ? screens.length - 1 : _counter - 1;
+    this.swipeTo(newCounter);
+  }
+
+  swipeTo(counter) {
+    const newColor = screens[counter].bgcolor;
+    this.setState({
+      _counter: counter
+    }, () => {
+      this.circleTransition.start(newColor, this.changeColor.bind(this, newColor));
+    });
+  }
+
+  onSwipe(gestureName, gestureState) {
+    const { SWIPE_UP, SWIPE_DOWN, SWIPE_LEFT, SWIPE_RIGHT } = swipeDirections;
+    this.setState({ gestureName: gestureName });
+    switch (gestureName) {
+      case SWIPE_LEFT:
+        this.onSwipeLeft();
+        break;
+      case SWIPE_RIGHT:
+        this.onSwipeRight();
+        break;
+    }
   }
 
   render() {
-    let topPosition = this.getTopPosition();
-    let leftPosition = this.getLeftPosition();
+    const config = {
+      velocityThreshold: 0.3,
+      directionalOffsetThreshold: 80
+    };
+
     return (
-      <TouchableWithoutFeedback onPress={this.onPress.bind(this)}>
-      <View style={styles.container}>
-        <Animated.View style={{
-          position: 'absolute',
-          backgroundColor: '#ccc',
-          width: size,
-          height: size,
-          borderRadius: size / 2,
-          transform: [{
-            scale: this.state.scale
-          }],
-          left: leftPosition,
-          top: topPosition
-        }} />
-      </View>
-      </TouchableWithoutFeedback>
+      <GestureRecognizer onPress={this.onPress.bind(this)}
+        style={[styles.container, { backgroundColor: this.state.currentbg }]}
+        onSwipe={(direction, state) => this.onSwipe(direction, state)}
+        config={config}
+      >
+        <CircleTransition
+          ref={(circle) => { this.circleTransition = circle }}
+        />
+      </GestureRecognizer>
     );
   }
 }
