8f93ce60039d3cb0ea51180e40fffa2564758f26
FRAMGIA\nguyen.manh.duc <nguyen.manh.duc@framgia.com>
2018-08-09 16:08:22 +0700

    init swipe

--- App.js
+++ App.js
@@ -7,6 +7,8 @@ import {
   TouchableWithoutFeedback,
   Easing
 } from 'react-native';
+
+import GestureRecognizer, { swipeDirections } from 'react-native-swipe-gestures';
 import CircleTransition from './src/components/CircleTransition.js';
 
 const { width, height } = Dimensions.get('window');
@@ -47,17 +49,67 @@ export default class App extends React.Component {
     });
   }
 
+  onSwipeLeft() {
+    console.log("left");
+    const { _counter } = this.state;
+    let newCounter = _counter < screens.length - 1 ? _counter + 1 : 0;
+    this.swipeTo(newCounter);
+  }
+  onSwipeRight() {
+    console.log("right");
+    const { _counter } = this.state;
+    let newCounter = _counter === 0 ? screens.length - 1 : _counter - 1;
+    this.swipeTo(newCounter);
+  }
+
+  swipeTo(counter) {
+    const newColor = screens[counter].bgcolor;
+    this.setState({
+      _counter: counter
+    }, () => {
+      this.circleTransition.start(newColor, this.changeColor.bind(this, newColor));
+    });
+  }
+
+  onSwipe(gestureName, gestureState) {
+    const { SWIPE_UP, SWIPE_DOWN, SWIPE_LEFT, SWIPE_RIGHT } = swipeDirections;
+    this.setState({ gestureName: gestureName });
+    switch (gestureName) {
+      case SWIPE_LEFT:
+        this.onSwipeLeft();
+        break;
+      case SWIPE_RIGHT:
+        this.onSwipeRight();
+        break;
+    }
+  }
+
   render() {
+    const config = {
+      velocityThreshold: 0.3,
+      directionalOffsetThreshold: 80
+    };
+
     return (
+      // <GestureRecognizer onPress={this.onPress.bind(this)}
+      //   style={[styles.container, { backgroundColor: this.state.currentbg }]}
+      //   onSwipe={(direction, state) => this.onSwipe(direction, state)}
+      //   config={config}
+      // >
+      //   <CircleTransition
+      //     ref={(circle) => { this.circleTransition = circle }}
+      //   />
+      // </GestureRecognizer>
+
       <TouchableWithoutFeedback onPress={this.onPress.bind(this)}>
-      <View style={[styles.container, {
-        backgroundColor: this.state.currentbg
-      }]}>
-        <CircleTransition
-          ref={(circle) => { this.circleTransition = circle }}
-        />
-      </View>
-    </TouchableWithoutFeedback>
+        <View style={[styles.container, {
+          backgroundColor: this.state.currentbg
+        }]}>
+          <CircleTransition
+            ref={(circle) => { this.circleTransition = circle }}
+          />
+        </View>
+      </TouchableWithoutFeedback>
     );
   }
 }
